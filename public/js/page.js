1e46ff2abc3d74050d822bbbc8cacd8d043b5ca0
Hemangi Likhia <n01380076@nj-00118-013.humber.org>
2020-02-24 18:44:23 -0500

    Created Home Page for Humber Media Pros

---
+++ public/js/page.js
@@ -0,0 +1,7 @@
+export default class Page {
+	constructor() {
+	}
+	getHTML() {
+		return this.html
+	}
+}
---
+++ public/js/pages/student.js
@@ -0,0 +1,8 @@
+import Page from '../page.js'
+
+export default class Student extends Page {
+	constructor() {
+		super()
+		this.html = '<h2>Student Page</h2>'
+	}
+}
